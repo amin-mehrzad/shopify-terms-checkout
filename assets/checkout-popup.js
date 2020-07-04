e212648d6d0e08924c14b633f40cab270cde30a2
unknown <amin.mehrzad@gmail.com>
2020-07-04 04:22:43 -0700

    click listener added to checkout drawer

--- assets/checkout-popup.js
+++ assets/checkout-popup.js
@@ -1,10 +1,33 @@
 
-window.onload = function() {
+window.onload = function () {
     //  alert('Page is loaded');
-      var checkoutDrawerButton = document.body.getElementsByClassName('btn btn--full cart__checkout drawer-button')[0];
-    
-      console.log(checkoutDrawerButton);
-    
-    //if (checkoutDrawerButton.clicked == true)
-     // alert('test');
-    };
+
+
+
+
+    var bodyElement = document.getElementsByTagName('body')[0];
+    var bodyLastClassName = bodyElement.className;
+    window.setInterval(function () {
+        //  console.log('hi');
+
+        var bodyClassName = bodyElement.className;
+        if (bodyClassName !== bodyLastClassName) {
+
+            setTimeout(function(){ // check again in a second
+
+            var checkoutDrawerButton = document.body.getElementsByClassName('btn btn--full cart__checkout drawer-button')[0];
+
+            console.log(checkoutDrawerButton);
+
+            checkoutDrawerButton.onclick = function() {
+                alert("button was clicked");
+             }​;
+
+            // if (checkoutDrawerButton.clicked == true)
+            //     alert('test');
+             } , 1000);
+
+            // bodyLastClassName = bodyClassName;
+        }
+    }, 10);
+};
