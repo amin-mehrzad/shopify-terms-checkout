e48ad7e97346169c77bcafa6febcab04b7ed18f7
unknown <amin.mehrzad@gmail.com>
2020-07-04 01:23:04 -0700

    pop-up script added to head

---
+++ assets/checkout-popup.js
@@ -0,0 +1,10 @@
+
+window.onload = function() {
+    //  alert('Page is loaded');
+      var checkoutDrawerButton = document.body.getElementsByClassName('btn btn--full cart__checkout drawer-button')[0];
+    
+      console.log(checkoutDrawerButton);
+    
+    //if (checkoutDrawerButton.clicked == true)
+     // alert('test');
+    };
