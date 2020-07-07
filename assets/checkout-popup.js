183137bf532623a2a729b223a35d25fabe734315
unknown <amin.mehrzad@gmail.com>
2020-07-07 02:54:37 -0700

    checkout drawer functionality done

--- assets/checkout-popup.js
+++ assets/checkout-popup.js
@@ -1,38 +1,43 @@
 
 window.onload = function () {
-    //  alert('Page is loaded');
+  //  alert('Page is loaded');
 
 
 
 
-    var bodyElement = document.getElementsByTagName('body')[0];
-    var bodyLastClassName = bodyElement.className;
-  	var checkoutTermModal = document.getElementById('checkout-popup-background');
-    window.setInterval(function () {
-        //  console.log('hi');
+  var bodyElement = document.getElementsByTagName('body')[0];
+  var bodyLastClassName = bodyElement.className;
+  var checkoutTermModal = document.getElementById('checkout-popup-background');
+var termAgreed=false;
+  window.setInterval(function () {
+      //  console.log('hi');
 
-        var bodyClassName = bodyElement.className;
-        if (bodyClassName !== bodyLastClassName) {
+      var bodyClassName = bodyElement.className;
+      if (bodyClassName !== bodyLastClassName) {
 
-            setTimeout(function(){ // check again in a second
+          setTimeout(function(){ // check again in a second
 
-            var checkoutDrawerButton = document.body.getElementsByClassName('btn btn--full cart__checkout drawer-button')[0];
+          var checkoutDrawerButton = document.body.getElementsByClassName('btn btn--full cart__checkout drawer-button')[0];
 
-          //  console.log(checkoutDrawerButton);
+        //  console.log(checkoutDrawerButton);
 
-            checkoutDrawerButton.onclick = function(e) {
-             //  here--->  alert("button was clicked");
-              e.preventDefault();
-              checkoutTermModal.style.display = "block";
+         checkoutDrawerButton.onclick = function(e) {
+           //  here--->  alert("button was clicked");
+           if(!termAgreed){
+            e.preventDefault();
+            checkoutTermModal.style.display = "block";
+                            // e.returnValue = true;
 
+           }
+           console.log(termAgreed);
+termAgreed=true;
+           }
 
-             }
+          // if (checkoutDrawerButton.clicked == true)
+          //     alert('test');
+           } , 1000);
 
-            // if (checkoutDrawerButton.clicked == true)
-            //     alert('test');
-             } , 1000);
-
-            // bodyLastClassName = bodyClassName;
-        }
-    }, 10);
+          // bodyLastClassName = bodyClassName;
+      }
+  }, 10);
 };
